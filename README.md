222d95f890233218a7268f8f961af15ade7d9c42
lincxx <bossc@BOSSCO>
2023-12-16 08:38:36 -0500

    Created partails for index. Created a new helper function loadView

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple job listing app. This app is built with vanilla php, html, tailwinds, and mysql
--- helpers.php
+++ helpers.php
@@ -3,8 +3,21 @@
 /**
  * Get the base path
  * 
- * @params string $path
+ * @param string $path
  */
+
 function basePath($path=''){
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+
+ function loadView($name){
+    require basePath("views/{$name}.view.php");
+ }
+
