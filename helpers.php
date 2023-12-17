e0235f08b1b86ced7e593d30869ccd5f398b568a
lincxx <bossc@BOSSCO>
2023-12-17 14:51:20 -0500

    Add in some more helper functions inspect and inspectAndDie

--- helpers.php
+++ helpers.php
@@ -19,6 +19,7 @@ function basePath($path=''){
 
  function loadView($name){
    $viewPath = basePath("views/{$name}.view.php");
+
    if(file_exists($viewPath)) {
     require $viewPath;
    } else {
@@ -42,4 +43,30 @@ function basePath($path=''){
    } else {
     echo "View '{$name}' not found!";
    }
+ }
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+
+ function inspect($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+ }
+
+ /**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+
+ function inspectAndDie($value) {
+    echo '<pre>';
+    die(var_dump($value));    
+    echo '</pre>';
  }
