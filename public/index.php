b6104fe1852c659db09069579238a83364f00492
lincxx <bossc@BOSSCO>
2023-12-18 06:43:49 -0500

    add in a very basic router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,22 @@
 <?php 
    require '../helpers.php';
-   //loads the home view
-   loadView('home');
+
+   $routes = [
+      '/' => 'controllers/home.php',
+      '/listings' => 'controllers/listings/index.php',
+      '/listings/create' => 'controllers/listings/create.php',
+      '404' => 'controllers/error/404.php'
+   ];
+
+
+   //get the current uri
+   $uri = $_SERVER['REQUEST_URI'];
+
+   //check to see if the uri is in the route array , then require that 
+
+   if(array_key_exists($uri, $routes)) {
+      require basePath($routes[$uri]);
+   } else {
+      require basePath($routes['404']);
+   }
 ?>
