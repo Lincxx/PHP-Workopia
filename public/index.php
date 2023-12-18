99581e11037ad2e4c0a188a7ac1f8a934dd82172
lincxx <bossc@BOSSCO>
2023-12-18 10:10:04 -0500

    refactor router to class

--- public/index.php
+++ public/index.php
@@ -1,8 +1,17 @@
 <?php 
    require '../helpers.php';
 
+   require basePath('Router.php');
+
+   $router = new Router();
+
+   
+   $routes = require basePath('routes.php');
+
    //get the current uri
    $uri = $_SERVER['REQUEST_URI'];
+   $method = $_SERVER["REQUEST_METHOD"];
 
-   require basePath('router.php');
+   $router->route($uri, $method);
+
 ?>
